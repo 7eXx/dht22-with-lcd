75290bf32246567520acee34369ad480c2933de9
Texx <marco.tex@live.com>
2025-05-22 22:27:38 +0200

    wip: change display logic. Remove delay in favor of time difference.

--- dht22-with-lcd.ino
+++ dht22-with-lcd.ino
@@ -2,6 +2,8 @@
 #include <DHT.h>
 #include <LiquidCrystal.h>
 
+#define BUTTON_PIN 13
+
 #define DHTPIN 8
 #define DHTTYPE DHT22
 
@@ -10,27 +12,50 @@ DHT dht(DHTPIN, DHTTYPE);
 const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
-uint32_t delayMS;
+uint8_t menuSelected;
+
+unsigned long previousMillis;
+unsigned long delayMS;
 float hum;  //Variabile in cui verrà inserita la % di umidità  
 float temp; //Variabile in cui verrà inserita la temperatura    
 
 void setup() {
+  pinMode(BUTTON_PIN, INPUT);
+  attachInterrupt(digitalPinToInterrupt(BUTTON_PIN), changeDisplayMenu, RISING);
+
   Serial.begin(9600);
   dht.begin();
   lcd.begin(16,2);
   lcd.clear();
+  menuSelected = 0;
 
+  previousMillis = 0;
   delayMS = 2000;
 }
 
 void loop() {
-  delay(delayMS);
-  // read humidity and temperature
-  hum = dht.readHumidity();
-  temp = dht.readTemperature();
+  changeReadValues();
   
   writeTemperature();
   writeHumidity();
+}
+
+void changeReadValues() {
+  unsigned long currentMillis = millis();
+  if (currentMillis - previousMillis >= delayMS) {
+    previousMillis = currentMillis;
+    // read humidity and temperature
+    hum = dht.readHumidity();
+    temp = dht.readTemperature();
+  }
+}
+
+void changeDisplayMenu() {
+  menuSelected++;
+  menuSelected = menuSelected % 3;  
+
+  Serial.print("Current selection: ");
+  Serial.println(menuSelected);
 }
 
 void writeTemperature() {
